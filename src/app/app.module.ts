4846bce97d8efe138a59bc5739e54a07e35d66a6
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-06 07:26:06 +0530

    store setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { bookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ book: bookReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
