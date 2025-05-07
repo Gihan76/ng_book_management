4f3307d46cb1bdf6818e3454aa18d5beb455dc60
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-07 07:12:33 +0530

    added 2 new actions to handle success and failure add book scernarios with reducers

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,14 +1,27 @@
 import { createReducer, on } from "@ngrx/store";
-import { AddBook, RemoveBook } from "./book.actions";
+import { AddBook, AddBookSuccess, AddBookFailure, RemoveBook } from "./book.actions";
 import { Book } from "../models/book";
 
 export const initialState: Book[] = [];
 
 export const bookReducer = createReducer(
     initialState,
-    on(AddBook, (state, { id, title, author }) =>
-        [...state, { id, title, author }]
-    ),
+
+    // intial add book action which returns current state
+    on(AddBook, (state) => { return state }),
+
+    // based on the Add Book action result -> success (AddBookSuccess) will trigger
+    on(AddBookSuccess, (state, { id, title, author }) => [...state, { id, title, author }]),
+
+    /**
+     * based on the Add Book action result -> failure (AddBookFailure) will trigger 
+     * which returns the current state unchanged with error
+     */
+    on(AddBookFailure, (state, { error}) => { 
+        console.error(error); 
+        return state;
+    }),
+
     on(RemoveBook, (state, { bookId }) =>
         state.filter(book => book.id !== bookId)
     ),
